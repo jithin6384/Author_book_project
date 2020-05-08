cd136ae68fe53055e1d00f532b3cd24f6fb32cb9
jithin <jeets2689@gmail.com>
2020-05-08 17:03:25 +0530

    added dob to author

---
+++ db/migrate/20200508111841_add_column_to_authors.rb
@@ -0,0 +1,5 @@
+class AddColumnToAuthors < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :authors, :dob, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_101650) do
+ActiveRecord::Schema.define(version: 2020_05_08_111841) do
 
   create_table "author_books", force: :cascade do |t|
     t.integer "book_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_101650) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "dob"
   end
 
   create_table "books", force: :cascade do |t|
